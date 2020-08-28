c7de47d86d550a59ced7ff652e3ad8984fe1c202
Dadang NH <project@dadangnh.com>
2020-08-28 23:54:16 +0700

    Add Debugger Package

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
