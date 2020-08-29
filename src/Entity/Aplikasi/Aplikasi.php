1201f48c8205a959cb4e7ef9f347ca0e9565c03a
Dadang NH <project@dadangnh.com>
2020-08-30 01:54:18 +0700

    Add Aplikasi Entity

---
+++ src/Entity/Aplikasi/Aplikasi.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Entity\Aplikasi;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\Aplikasi\AplikasiRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\UuidInterface;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=AplikasiRepository::class)
+ */
+class Aplikasi
+{
+    /**
+     * @var UuidInterface
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\CustomIdGenerator(class=UuidGenerator::class)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nama;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $systemName;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createDate;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $deskripsi;
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+
+    public function getNama(): ?string
+    {
+        return $this->nama;
+    }
+
+    public function setNama(string $nama): self
+    {
+        $this->nama = $nama;
+
+        return $this;
+    }
+
+    public function getSystemName(): ?string
+    {
+        return $this->systemName;
+    }
+
+    public function setSystemName(string $systemName): self
+    {
+        $this->systemName = $systemName;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreateDate(): ?\DateTimeImmutable
+    {
+        return $this->createDate;
+    }
+
+    public function setCreateDate(\DateTimeImmutable $createDate): self
+    {
+        $this->createDate = $createDate;
+
+        return $this;
+    }
+
+    public function getDeskripsi(): ?string
+    {
+        return $this->deskripsi;
+    }
+
+    public function setDeskripsi(?string $deskripsi): self
+    {
+        $this->deskripsi = $deskripsi;
+
+        return $this;
+    }
+}
