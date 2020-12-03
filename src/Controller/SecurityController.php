64b2b9ebaac466388a8b050f1103b723bc73fff1
Dadang NH <project@dadangnh.com>
2020-12-03 15:38:49 +0700

    Change json login to response more data

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,10 +15,6 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
@@ -37,11 +33,19 @@ class SecurityController extends AbstractController
 
     /**
      * @Route("/json_login", name="app_json_login", methods={"POST"})
+     * @param IriConverterInterface $iriConverter
+     * @return Response
      */
     public function json_login(IriConverterInterface $iriConverter)
     {
-        return new Response(null, 204, [
-            'Location' => $iriConverter->getIriFromItem($this->getUser())
+        return $this->json([
+            'user' => $this->getUser() ? $iriConverter->getIriFromItem($this->getUser()) : null,
+            'username' => $this->getUser() ? $this->getUser()->getUsername() : null,
+            'role' => $this->getUser() ? $this->getUser()->getRoles() : null,
+            'pegawai' => $this->getUser()->getPegawai() ? $iriConverter->getIriFromItem($this->getUser()->getPegawai()) : null,
+            'nama' => $this->getUser()->getPegawai() ? $this->getUser()->getPegawai()->getNama() : null,
+            'nip9' => $this->getUser()->getPegawai() ? $this->getUser()->getPegawai()->getNip9() : null,
+            'nip18' => $this->getUser()->getPegawai() ? $this->getUser()->getPegawai()->getNip18() : null,
         ]);
     }
 }
