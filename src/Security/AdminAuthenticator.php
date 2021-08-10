0c97d43b6b209c06f00f9b567cd740af2dc767b3
Dadang NH <project@dadangnh.com>
2021-08-10 09:17:07 +0700

    Add redirect on authentication success without referrer

--- src/Security/AdminAuthenticator.php
+++ src/Security/AdminAuthenticator.php
@@ -51,8 +51,8 @@ class AdminAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('api_doc'));
+//        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
